3d647f5da74bc451f9ed6019f27ba74b3e5f4fb8
floreaaa <florea.user@gmail.com>
2024-06-19 10:55:27 +0300

    Show active link properly.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -34,7 +34,7 @@ const showingNavigationDropdown = ref(false);
                                 </NavLink>
                             </div>
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('upload')" :active="route().current('dashboard')">
+                                <NavLink :href="route('upload')" :active="route().current('upload')">
                                     Upload prayer
                                 </NavLink>
                             </div>
